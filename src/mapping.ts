96385bb2e12f21103c176539a051fb28b1628081
dazhlord <dazhlord@gmail.com>
2021-04-25 11:30:58 +0200

    index the amount of the token owned by the user

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,5 +1,6 @@
 import { Transfer } from '../generated/Rarible/Rarible'
-import { RaribleOwner } from '../generated/schema'
+import { RaribleOwner, RaribleBalance } from '../generated/schema'
+import { BigInt } from "@graphprotocol/graph-ts"
 
 export function handleTransfer(event: Transfer): void {
     let id = event.params.tokenId.toHex()
@@ -9,4 +10,25 @@ export function handleTransfer(event: Transfer): void {
     }
     rarible.owner = event.params.to
     rarible.save()
+
+    let previousOwner = event.params.from.toHex()
+    let raribleBalance = RaribleBalance.load(previousOwner)
+    if (raribleBalance != null) {
+        if (raribleBalance.amount > BigInt.fromI32(0)) {
+            raribleBalance.amount = raribleBalance.amount - BigInt.fromI32(1)
+        }
+    } else {
+        raribleBalance = new RaribleBalance(previousOwner)
+        raribleBalance.amount = BigInt.fromI32(0)
+    }
+    raribleBalance.save()
+
+    let newOwner = event.params.to.toHex()
+    let newRaribleBalance = RaribleBalance.load(newOwner)
+    if (newRaribleBalance == null) {
+        newRaribleBalance = new RaribleBalance(newOwner)
+        newRaribleBalance.amount = BigInt.fromI32(0)
+    }
+    newRaribleBalance.amount = newRaribleBalance.amount + BigInt.fromI32(1)
+    newRaribleBalance.save()
 }
