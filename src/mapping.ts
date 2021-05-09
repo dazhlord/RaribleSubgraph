0824498a31ceab8223038662893508307af2e19b
dazhlord <dazhlord@gmail.com>
2021-05-09 20:28:23 +0200

    consider more attributes in the entity

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,14 +1,16 @@
 import { Transfer } from '../generated/Rarible/Rarible'
 import { RaribleOwner, RaribleBalance } from '../generated/schema'
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, Address } from "@graphprotocol/graph-ts"
 
 export function handleTransfer(event: Transfer): void {
-    let id = event.params.tokenId.toHex()
+    let id = event.transaction.hash.toHex()
     let rarible = RaribleOwner.load(id)
     if (rarible == null) {
         rarible = new RaribleOwner(id)
     }
+    rarible.tokenId = event.params.tokenId
     rarible.owner = event.params.to
+    rarible.contract = Address.fromString("0x60f80121c31a0d46b5279700f9df786054aa5ee5")
     rarible.save()
 
     let previousOwner = event.params.from.toHex()
