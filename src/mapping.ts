4ce769bba76c9a89bae3d50683e9e264c35d2741
dazhlord <dazhlord@gmail.com>
2021-05-13 21:20:24 +0200

    - correct the data collection - added a new entitiy TransferTrace

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,33 +1,49 @@
-import { Transfer } from '../generated/Rarible/Rarible'
-import { RaribleOwner, RaribleBalance } from '../generated/schema'
-import { BigInt, Address } from "@graphprotocol/graph-ts"
+import { Transfer, Rariable } from '../generated/Rarible/Rarible'
+import { RaribleOwner, RaribleBalance, TransferTrace } from '../generated/schema'
+import { BigInt } from "@graphprotocol/graph-ts"
 
 export function handleTransfer(event: Transfer): void {
-    let id = event.transaction.hash.toHex()
-    let rarible = RaribleOwner.load(id)
-    if (rarible == null) {
-        rarible = new RaribleOwner(id)
-    }
+    //let id = event.transaction.hash.toHex()
+    let id = event.params.tokenId.toHex()
+    let rarible = new RaribleOwner(id)
+    //the loading way is much slower
+    // let rarible = RaribleOwner.load(id)
+    // if (rarible == null) {
+    //     rarible = new RaribleOwner(id)
+    // }
     rarible.tokenId = event.params.tokenId
     rarible.owner = event.params.to
     rarible.contract = event.address
     rarible.save()
 
-    let previousOwner = event.params.from.toHex()
-    let raribleBalance = RaribleBalance.load(previousOwner)
-    if (raribleBalance != null) {
-        if (raribleBalance.amount > BigInt.fromI32(0)) {
-            raribleBalance.amount = raribleBalance.amount - BigInt.fromI32(1)
-        }
-        raribleBalance.save()
-    } 
+    //collect the entities of transfer traces
+    let transferEntity = new TransferTrace(event.transaction.hash.toHex())
+    transferEntity.from = event.params.from
+    transferEntity.to = event.params.to
+    transferEntity.timestamp = event.block.timestamp
+    transferEntity.save()
+
+    //update the amount of the token hold by the owner "to"
+    let contract = Rariable.bind(event.address)
+    let raribleBalance = new RaribleBalance(event.params.to.toHex())
+    raribleBalance.amount = contract.balanceOf(event.params.to)
+    raribleBalance.save()
+
+    // let previousOwner = event.params.from.toHex()
+    // let raribleBalance = RaribleBalance.load(previousOwner)
+    // if (raribleBalance != null) {
+    //     if (raribleBalance.amount > BigInt.fromI32(0)) {
+    //         raribleBalance.amount = raribleBalance.amount - BigInt.fromI32(1)
+    //     }
+    //     raribleBalance.save()
+    // } 
     
-    let newOwner = event.params.to.toHex()
-    let newRaribleBalance = RaribleBalance.load(newOwner)
-    if (newRaribleBalance == null) {
-        newRaribleBalance = new RaribleBalance(newOwner)
-        newRaribleBalance.amount = BigInt.fromI32(0)
-    }
-    newRaribleBalance.amount = newRaribleBalance.amount + BigInt.fromI32(1)
-    newRaribleBalance.save()
+    // let newOwner = event.params.to.toHex()
+    // let newRaribleBalance = RaribleBalance.load(newOwner)
+    // if (newRaribleBalance == null) {
+    //     newRaribleBalance = new RaribleBalance(newOwner)
+    //     newRaribleBalance.amount = BigInt.fromI32(0)
+    // }
+    // newRaribleBalance.amount = newRaribleBalance.amount + BigInt.fromI32(1)
+    // newRaribleBalance.save()
 }
