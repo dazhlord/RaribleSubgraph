b8ccd81287049e366a55c4aee59b85cc674d04c8
dazhlord <dazhlord@gmail.com>
2021-05-22 14:56:50 +0200

    use template to support multiple networks

--- src/mapping.ts
+++ src/mapping.ts
@@ -3,14 +3,12 @@ import { RaribleOwner, RaribleBalance, TransferTrace } from '../generated/schema
 import { BigInt, log } from "@graphprotocol/graph-ts"
 
 export function handleTransfer(event: Transfer): void {
-    //let id = event.transaction.hash.toHex()
     let id = event.params.tokenId.toHex()
-    let rarible = new RaribleOwner(id)
-    //the loading way is much slower
-    // let rarible = RaribleOwner.load(id)
-    // if (rarible == null) {
-    //     rarible = new RaribleOwner(id)
-    // }
+    //let rarible = new RaribleOwner(id)
+    let rarible = RaribleOwner.load(id)
+    if (rarible == null) {
+      rarible = new RaribleOwner(id)
+    }
     rarible.tokenId = event.params.tokenId
     rarible.owner = event.params.to
     rarible.contract = event.address
